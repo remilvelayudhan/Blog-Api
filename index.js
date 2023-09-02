a46ce07d1f785ac1bdcd4115e55d8e02478d69f2
cvremil1 <remil.cv@infolitz.com>
2023-09-03 00:20:51 +0530

    db connection

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT ;
+
+//require (./db);
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World! welcom to blg api')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
